b1004522407fdfe2146af3f13da8f994f70288b3
Jon Williams <jon.williams@warnerbros.com>
2019-07-09 10:51:20 +0200

    Adds some helpful comments

--- R/basic.R
+++ R/basic.R
@@ -11,7 +11,7 @@
 #' @examples
 #' add(5, 10)
 #' ## Can also use negative numbers
-#' add(-5, 10)
+#' add(-5, 10) # Should give 5
 add = function(x, y) {
   if (is.na(x) || is.na(y)) {
     message("Bad luck, you've got some NAs. I hope it's not catching.")
